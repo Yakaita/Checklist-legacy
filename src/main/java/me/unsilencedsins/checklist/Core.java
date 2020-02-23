52d9337ab3efa88ce283a99ef3bd3c9cc13f0b2e
Jeremy Desktop <42159319+UnSilencedSins@users.noreply.github.com>
2020-02-22 21:49:47 -0500

    testing

---
+++ src/main/java/me/unsilencedsins/checklist/Core.java
@@ -0,0 +1,18 @@
+package me.unsilencedsins.checklist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        //this is some stuff here
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
